3927d0632acc0edf00966193ba7923898c41b9e4
Augusto <augustomarinangeli91@gmail.com>
2023-03-10 13:27:37 -0300

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//parte 1
+        String texto1 = "Hola buenos dias, como estas?";
+        char letra = 'o';
+        int vecesLetra = 0;
+
+        for (int i=0; i<texto1.length();i++)
+
+            if (texto1.charAt(i)== letra) {
+                vecesLetra++;
+            }
+        System.out.println(vecesLetra);
+//parte 2
+
+        int numeros[] = new int[]{9, 14, 25};
+
+
+
+//parte 3
+
+            int numeros2[] = new int[] {5,7,9,12,15};
+            int numX = 8;
+            int suma = 0;
+
+            for (int i=0; i<numeros2.length; i++) {
+
+            if (numeros2[i]>numX) {
+                suma+= numeros2[i] ;
+            }
+        }
+        System.out.println(suma);
+
+            //parte 4
+
+            String fraseMover = "ahola que tal";
+            int desplazamiento = 2;
+
+            for (int i = 0; i < fraseMover.length(); i++) {
+                char caracter = fraseMover.charAt(i);
+        }
+        System.out.println(fraseMover);
+
+        }
     }
-}
+
+
+
